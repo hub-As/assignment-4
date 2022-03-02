2f2a312de6e9b09b9e67827fdddf5694bc7985dc
hub-As <97188055+hub-As@users.noreply.github.com>
2022-03-02 12:16:26 +0530

    Update output 4.py

--- output 4.py
+++ output 4.py
@@ -1,4 +1,4 @@
-#output1
+#output 1
 move disc 1 from A to c
 move disc 2 from A to B
 move disc 1 from c to B
@@ -9,15 +9,13 @@ move disc 1 from A to c
 None
 
 
-
 #output 2a
 enter number of rows5
     1     
    1 1    
   1 2 1   
  1 3 3 1  
-1 4 6 4 1 
-
+1 4 6 4 1
 
 
 #output 2b
@@ -32,32 +30,35 @@ enter number of rows 5
 
 #output 3
 enter first number(integer)- 5
-enter second number(integer)- 4
+enter second number(non zero integer)- 3
 Quotient is  1 
-remainder is  1
+remainder is  2
 is function calleable True
 is value calleable False
 all values are non zero
 values greater than 4
-{5, 6, 7}
-frozenset({5, 6, 7})
-maximum value is  7
-its hash value is  1691112344503229683
+[1, 2, 4, 5, 6]
+{ 5, 6}
+frozenset({5, 6})
+maximum value is  6
+its hash value is  6
 
 
-#question4
+
+#output 4
 Object Created...
 The Name Of The Student Is akshit And SID Is 21103042.
 Destructor called
 
 
-#question 5
+#output 5
 The Updated Salary Of Mehak Is 70000 
 Record Of Viren Deleted!
 
 
-#question 6
-enter George's word late
-enter Barbie's word tale
+
+#output 6
+enter George's word Tree
+enter Barbie's word reet
 is Barbie's word meaningful(Y/N) y
 their friendship is true
